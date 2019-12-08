0c0999d0075336cce30776cd5b6708ba7cc522a2
‘Andrei <andrei.dima@usm.ro>
2019-12-08 11:44:40 +0200

    'diverse'

--- app/Http/Controllers/PlataOnlineController.php
+++ app/Http/Controllers/PlataOnlineController.php
@@ -25,7 +25,8 @@ class PlataOnlineController extends Controller
         $rezervare = $request->session()->get('rezervare');
 
         $comanda = Mobilpay::setOrderId(md5(uniqid(rand())))
-        ->setAmount($rezervare->pret_total)
+        // ->setAmount($rezervare->pret_total)
+        ->setAmount('0.01')
         ->setCurrency($rezervare->currency)
         ->setReturnUrl($rezervare->return_url)
         ->setDetails('Plata online pentru biletul - ' . $rezervare->id)
