67a8468ec268ed3033340cdd53292dfdfa87c6ac
‘Andrei <andrei.dima@usm.ro>
2020-09-04 14:46:01 +0300

    'sms'

---
+++ app/SmsTrimis.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SmsTrimis extends Model
+{
+    protected $table = 'mesaje_trimise_sms';
+    protected $guarded = [];
+}
