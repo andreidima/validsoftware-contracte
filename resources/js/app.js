36fd2efffdab082f3b22fa9805b7daa2050c53d9
‘Andrei <andrei.dima@usm.ro>
2021-01-19 11:52:32 +0200

    'a'

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,15 +9,21 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
+ * Uncomment below when compiling to production
+ */
+// if (process.env.MIX_ENV_MODE === 'production') {
+    Vue.config.devtools = false;
+    Vue.config.debug = false;
+    Vue.config.silent = true;
+// }
+
+/**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('vue2-datepicker-miercuri', require('./components/DatePickerMiercuri.vue').default);
