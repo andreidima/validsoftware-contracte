46b5b740e3469d49d584587ecf023d1adde06312
‘Andrei <andrei.dima@usm.ro>
2021-04-05 13:54:49 +0300

    a

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
 
         App\Providers\GoogleDriveServiceProvider::class,
 
+        Intervention\Image\ImageServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -228,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
