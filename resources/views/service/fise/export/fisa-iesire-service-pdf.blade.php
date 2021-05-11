6c3f16fea46a223e0ad5716d5669a5c5334d2011
unknown <andrei.dima@usm.ro>
2021-05-11 21:50:47 +0300

    a

--- resources/views/service/fise/export/fisa-iesire-service-pdf.blade.php
+++ resources/views/service/fise/export/fisa-iesire-service-pdf.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Fișă service</title>
     <style>
-        html { 
+        html {
             margin: 0px 0px;
         }
 
-        body { 
+        body {
             font-family: DejaVu Sans, sans-serif;
             /* font-family: Arial, Helvetica, sans-serif; */
             font-size: 14px;
@@ -32,18 +32,18 @@
             width: 100%;
             word-wrap:break-word;
         }
-        
+
         th, td {
             padding: 1px 10px;
             border-width: 0px;
             border-style: solid;
-            
+
         }
         tr {
             border-style: solid;
             border-width: 0px;
         }
-        hr { 
+        hr {
             display: block;
             margin-top: 0.5em;
             margin-bottom: 0.5em;
@@ -51,18 +51,18 @@
             margin-right: auto;
             border-style: inset;
             border-width: 0.5px;
-        } 
+        }
     </style>
 </head>
 
 <body>
-    {{-- <div style="width:730px; height: 1030px; border-style: dashed ; border-width:2px; border-radius: 15px;">      --}}        
+    {{-- <div style="width:730px; height: 1030px; border-style: dashed ; border-width:2px; border-radius: 15px;">      --}}
     <img src="{{ asset('images/contract-header.jpg') }}" width="800px">
 
     <div style="
         /* border:dashed #999; */
-        width:710px; 
-        min-height:500px;            
+        width:710px;
+        min-height:500px;
         padding: 0px 0px 0px 0px;
         margin:20px 50px;
             -moz-border-radius: 10px;
@@ -70,7 +70,7 @@
             border-radius: 10px;">
 
 
-            @if ($fisa->consultanta_it === 1) 
+            @if ($fisa->consultanta_it === 1)
                 <p style="text-align: center; font-weight: bold; font-size: 21px;">FIȘĂ DE IEȘIRE CONSULTANȚĂ IT</p>
             @else
                 <p style="text-align: center; font-weight: bold; font-size: 21px;">FIȘĂ DE IEȘIRE DIN SERVICE</p>
@@ -92,7 +92,7 @@
                     (isset($fisa->client->reprezentant) ? ', Reprezentant ' . ($fisa->client->reprezentant) : '') .
                     (isset($fisa->client->reprezentant_functie) ? ', în funcția de ' . ($fisa->client->reprezentant_functie) : '') .
                     (isset($fisa->client->telefon) ? ', telefon: ' . ($fisa->client->telefon) : '') .
-                    (isset($fisa->client->email) ? ', email: ' . ($fisa->client->email) : '') . 
+                    (isset($fisa->client->email) ? ', email: ' . ($fisa->client->email) : '') .
                     (isset($fisa->client->site_web) ? ', site web: ' . ($fisa->client->site_web) : '')
                 }}.
             </p>
@@ -146,7 +146,7 @@
                         $html .= '</li>';
                     }
                     $html .='</ul>';
-                        
+
                     $html .= '<br />';
 
                 @endphp
@@ -163,14 +163,14 @@
                 <br />
             @endif
 
-            <br /><br />
+            {{-- <br /><br /> --}}
             <table align="center" style="width: 100%">
                 <tr>
                     <td style="width:50%" align="center"><b>Beneficiar,</b>
                         <br/> {{ $fisa->client->nume }}
                         <br /><br /> {{ $fisa->client->reprezentant_functie }}
                         <br /> {{ $fisa->client->reprezentant }}
-                    </td>                            
+                    </td>
                     <td style="width:50%" align="center"><b>Prestator,</b>
                         <br/>Dima P. Valentin PFA
                         <br/>
@@ -188,39 +188,39 @@
             <p style="page-break-after: always;"></p>
 
             {{-- Pagină publicitara --}}
-            
+
             <br><br><br>
 
             <p style="text-align:left; font-weight: bold;">Despre noi</p>
             <p style="text-align:justify;">
                 Suntem o firmă din Focșani, înființată în anul 2012, orientată pe dezvoltarea de servicii informatice și consultanță IT. Produsele informatice pe care le oferim acoperă atât clienți din sectorul public/ privat din România, cât și cei de pe piața internațională. Pentru mai multe detalii legate de activitatea noastră din zona de servicii web, vă invităm să accesați secțiunea Portofoliu de la adresa <a href="https://validsoftware.ro/" target="_blank">https://validsoftware.ro</a>.
             </p>
-            
+
             <br />
 
             <p style="text-align:left; font-weight: bold;">Echipă și scop</p>
             <p style="text-align:justify;">
-                Echipa noastră este formată din specialiști, absolvenți de studii superioare în domeniul IT, dar și în domenii conexe. Scopul nostru este furnizarea de servicii integrate, pentru a oferi clienților noștri creșterea competitivității și performanței activităților pe care le desfășoară.  
+                Echipa noastră este formată din specialiști, absolvenți de studii superioare în domeniul IT, dar și în domenii conexe. Scopul nostru este furnizarea de servicii integrate, pentru a oferi clienților noștri creșterea competitivității și performanței activităților pe care le desfășoară.
             </p>
 
             <br />
 
             <p style="text-align:left; font-weight: bold;">Tehnologie</p>
             <p style="text-align:justify;">
-                Adoptăm tehnologii de ultimă oră și ne bazăm pe spiritul de inovație al colegilor noștri. Oferim calitate și eficiență, finalizând cu succes proiectele, indiferent dacă acestea implică soluții simple sau complexe.  
+                Adoptăm tehnologii de ultimă oră și ne bazăm pe spiritul de inovație al colegilor noștri. Oferim calitate și eficiență, finalizând cu succes proiectele, indiferent dacă acestea implică soluții simple sau complexe.
             </p>
             <br />
 
             <p style="text-align:left; font-weight: bold;">Ce vă oferim</p>
                 <ul>Venim în întâmpinarea nevoilor dumneavoastră prin:<br><br>
                     <li>
-                        <b>Servicii web și multumedia</b>: achiziționare domenii, găzduire site-uri și aplicații web, dezvoltare aplicații web personalizate, realizare site-uri de prezentare și magazine online, promovare online servicii și produse, fotografii de produs/ locație și clipuri de prezentare, consultanță IT. 
+                        <b>Servicii web și multumedia</b>: achiziționare domenii, găzduire site-uri și aplicații web, dezvoltare aplicații web personalizate, realizare site-uri de prezentare și magazine online, promovare online servicii și produse, fotografii de produs/ locație și clipuri de prezentare, consultanță IT.
                     </li>
                     <li>
-                        <b>Service IT</b>: Instalare și actualizare sisteme de operare și aplicații adiacente, remedierea problemelor software ale calculatoarelor, salvare și transfer date, configurare echipamente IT, upgrade configurații hardware, mentenanță preventivă calculatoare. 
+                        <b>Service IT</b>: Instalare și actualizare sisteme de operare și aplicații adiacente, remedierea problemelor software ale calculatoarelor, salvare și transfer date, configurare echipamente IT, upgrade configurații hardware, mentenanță preventivă calculatoare.
                     </li>
                     <li>
-                        <b>Consultanță IT remote (de la distanță)</b>: Servicii de optimizare calculatoare, remediere probleme software de la distanță, consultanță IT generală, cu posibilitate de plată online cu cardul. 
+                        <b>Consultanță IT remote (de la distanță)</b>: Servicii de optimizare calculatoare, remediere probleme software de la distanță, consultanță IT generală, cu posibilitate de plată online cu cardul.
                     </li>
                 </ul>
             <br />
@@ -238,9 +238,8 @@
                 </li>
             </ul>
             <br />
-                    
+
     </div>
 </body>
 
 </html>
-    
