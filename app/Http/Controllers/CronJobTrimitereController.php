5d63afaa84319aeeac3b5560906c2834214e451d
‘Andrei <andrei.dima@usm.ro>
2021-10-18 15:33:29 +0300

    a

--- app/Http/Controllers/CronJobTrimitereController.php
+++ app/Http/Controllers/CronJobTrimitereController.php
@@ -13,12 +13,15 @@ class CronJobTrimitereController extends Controller
     public function trimitere(CronJob $cron_job, $key = null)
     {
         $config_key = \Config::get('variabile.cron_job_key');
-        // dd($key, $config_key);
+        // dd($key, $config_key, \Carbon\Carbon::now()->isoFormat('HH:00:00'));
 
         if ($key === $config_key){
             $cron_jobs = CronJob::all()
                 ->where('ziua', \Carbon\Carbon::now()->isoFormat('D'))
+                ->where('ora', \Carbon\Carbon::now()->isoFormat('HH:00:00'))
                 ->where('stare', 1);
+
+// dd($cron_jobs, \Carbon\Carbon::parse($cron_jobs->first()->ora)->isoFormat('HH'));
 
             foreach ($cron_jobs as $cron_job) {
                 if(isset($cron_job->client->email)) {
@@ -28,9 +31,9 @@ class CronJobTrimitereController extends Controller
                     // dd($cron_job->client->email, $to_email);
 
                     \Mail::
-                        to($to_email)
-                        // to('andrei.dima@usm.ro')
-                        ->bcc(['contact@validsoftware.ro', 'adima@validsoftware.ro'])
+                        // to($to_email)
+                        to('andrei.dima@usm.ro')
+                        // ->bcc(['contact@validsoftware.ro', 'adima@validsoftware.ro'])
                         // ->bcc(['adima@validsoftware.ro'])
                         ->send(new CronJobTrimitere($cron_job)
                     );
