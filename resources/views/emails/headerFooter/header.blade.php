c9616097de1b039df7ba028e0ac68d91ae20dcc3
Andrei Dima <andrei.dima@usm.ro>
2023-01-15 09:02:03 +0200

    a

--- resources/views/emails/headerFooter/header.blade.php
+++ resources/views/emails/headerFooter/header.blade.php
@@ -1,3 +1,3 @@
-<img src="{{ asset('images/email/logo-email.jpg') }}" style="padding:20px 0px;">
+<img src="{{ asset('images/email/logo-email-since.jpg') }}" style="padding:20px 0px;">
 
 <hr style="height: 1px; background-image: linear-gradient(to right, red, blue); margin:0px 20px;">
