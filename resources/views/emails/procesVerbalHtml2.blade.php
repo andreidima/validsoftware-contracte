7e4cf663911fe7b49c238e345a80f8c84f334c13
Andrei Dima <andrei.dima@usm.ro>
2023-01-24 22:21:43 +0200

    a

--- resources/views/emails/procesVerbalHtml2.blade.php
+++ resources/views/emails/procesVerbalHtml2.blade.php
@@ -5,7 +5,7 @@
         <div style="padding:20px 20px; max-width:760px!important;margin:0 auto; font-size:18px">
             Bună ziua, <b>{{ $procesVerbal->client->nume ?? '' }}</b>,
             <br><br>
-            @if ($procesVerbal->fisiere->count() === 0){
+            @if ($procesVerbal->fisiere->count() === 0)
                 Îți trimitem atașat documentul {{ $procesVerbal->titlu_document }} nr. {{ $procesVerbal->nr_document }} din
                     {{ (isset($procesVerbal->data_emitere) ? (\Carbon\Carbon::parse($procesVerbal->data_emitere)->isoFormat('DD.MM.YYYY')) : '') }}.
             @else
