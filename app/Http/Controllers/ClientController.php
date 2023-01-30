57aab7ae1822a684a253a3387c9191220b99cd84
‘Andrei <andrei.dima@usm.ro>
2023-01-30 13:32:25 +0200

    a

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -44,9 +44,8 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        $client = Client::make($this->validateRequest($request));
-        $client->save();
-dd($client, $client->path());
+        $client = Client::create($this->validateRequest($request));
+// dd($client, $client->path());
         return redirect($client->path())->with('status', 'Clientul "' . $client->nume . '" a fost adăugat cu succes!');
     }
 
