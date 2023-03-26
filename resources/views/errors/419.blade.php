2f995e0bde0d47b607c84a9a877df536927b136c
Andrei Dima <andrei.dima@usm.ro>
2023-03-26 09:29:01 +0300

    a

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6 mb-5">
+            <div class="card bg-warning">
+                <div class="card-header">SYSTEM ERROR</div>
+
+                <div class="card-body text-center">
+                    419 | PAGE EXPIRED
+                    <br>
+                    <br>
+                    <a class="btn btn-primary border border-dark rounded-3" href="{{ url('/') }}">
+                        Revino la pagina principală
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
