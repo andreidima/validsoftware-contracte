d5fcf5058b70f99dadd0a799d3ff57dd429ff768
‘Andrei <andrei.dima@usm.ro>
2023-07-11 10:18:09 +0300

    a

--- resources/views/chatGPT/produse/diverse/raspunsInterogareOAI.blade.php
+++ resources/views/chatGPT/produse/diverse/raspunsInterogareOAI.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.app')
 
 <script type="application/javascript">
-    deCopiat={!! json_encode($promptTrimis) !!}
+    deCopiat={!! json_encode($promptPentruCopyToClipboard) !!}
 </script>
 
 @section('content')
